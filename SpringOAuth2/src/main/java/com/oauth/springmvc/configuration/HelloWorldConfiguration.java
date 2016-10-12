368186c1655a40ff2dfb94bba11cd5186312f027
avinash-chopra <avinash.chopra@metacube.com>
2016-10-12 13:54:16 +0530

    OAuth custom login

---
+++ SpringOAuth2/src/main/java/com/oauth/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.oauth.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.oauth.springmvc")
+public class HelloWorldConfiguration {
+	
+
+}
